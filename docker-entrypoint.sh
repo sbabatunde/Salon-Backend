73482d231f4d73244570b395e4f7b533a3b20d91
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-03-10 16:27:41 +0100

    seed error clear

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+php artisan config:clear
+php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
-php artisan db:seed --force  # ← Add this line
+php artisan db:seed --force   
 
 apache2-foreground
