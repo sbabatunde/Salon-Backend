39d0284926666e0cb9e08692bb569f9382c41507
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-03-10 17:05:49 +0100

    entrypoint update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,15 @@
 set -e
 
 php artisan config:clear
+
+# Migrate FIRST to create tables
+php artisan migrate --force
+
+# Then clear cache (table now exists)
 php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force
-php artisan db:seed --force   
+php artisan db:seed --force
 
 apache2-foreground
