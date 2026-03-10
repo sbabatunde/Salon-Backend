ba7ae7f083b44377f1e629024b8bdcb1abe907dc
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-03-10 16:18:19 +0100

    migrate seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-# Cache config
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Run migrations
 php artisan migrate --force
+php artisan db:seed --force  # ← Add this line
 
-# Start Apache
 apache2-foreground
