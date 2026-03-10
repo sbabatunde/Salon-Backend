8bc89d0cc706298e01ff11aa26d0a9e66f90e0cc
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-03-10 13:18:20 +0100

    Fix: rename dockerFile to Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-# Ensure SQLite database exists
-mkdir -p /var/www/html/storage/app
-touch /var/www/html/storage/app/precious_hairmpire.sqlite
-chown -R www-data:www-data /var/www/html/storage
-
-# Optimization
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Run migrations
 php artisan migrate --force
 
-# Start Apache in the foreground
-exec apache2-foreground
+apache2-foreground
